5c8fb0285e33a8ccde18a9145db2531c579d6d9f
omnijxs <juri.surakka@gmail.com>
2020-10-09 10:57:22 +0300

    Add more complex test cases.

--- test/yourTest.js
+++ test/yourTest.js
@@ -11,34 +11,50 @@ let data = [];
 describe('PLAYER: Complex operations', () => {
 
   /** 
-   * Implement the following TESTS and endpoint:
+   * Implement the following TESTS and ENDPOINTS:
    * 
-   * 1.
-   * An endpoint which returns all players sorted by score in descending order.
+   * 1. An endpoint which returns all players sorted by score in descending order.
    * 
-   * 2.
-   * An endpoint which updates all players with a given team name to another team name.
-   * For example, all those with a team name RED changed to PURPLE.
+   * 2. An endpoint which updates all players with a given team name to another team name.
+   *    For example, all those with a team name RED changed to PURPLE.
    *
-   * 3. 
-   * An endpoint which returns players grouped by team. 
-   * response should be. you might want to create a new class in the data folder.
-   * Score calculated
+   * 3. An endpoint which returns players grouped by team and team total score and sorted by score descending
+   *    The response data structure should be like:
    * 
-   * {
-  *       "teams" : [{
-  *               "name": "BANANA",
-  *               "totalScore": 110002,
-  *               "members": [{
-  *                   "name": "Gimli Gloin"
-  *                   }] 
-  *       ]
-  *       
-  *          
-    * }
-    * 
-    * 
-    *
-    */
+   *    "teams" : [
+   *        {
+   *          "name": "a", 
+   *          "totalScore": 11000
+   *        } ...
+   * 
+   *      ]
+   * 
+   * Protip 1: You have to implement the calculation of total score
+   * Protip 2: Create two new classes into data-folder which represent the return value.
+   
+   * 3. An endpoint which returns players grouped by team and team total score and sorted by score descending AND team member information
+   *    The response data structure should be like:
+   * 
+   *    "teams" : [
+   *        {
+   *          "name": "a", 
+   *          "totalScore": 11000
+   *          "members": [
+   *            {
+   *              "uuid": "uuid_10",
+   *              "name": "Malcolm Doe",
+   *              "createdAtYear": "2020"
+   *            }
+   *          ]
+   *        } ...
+   * 
+   *      ]
+   * 
+   * Protip 1: Use the same calculation logic for team score as in the above test case
+   * Protip 2: Think what data classes you can reuse from the above test case.
+   * Protip 3: Do not use the player.js class in the member.
+   *
+   *
+   */
 
 });
