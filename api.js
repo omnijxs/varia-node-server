4ab142b724d1bf6cb4c4f7848a2b9946d7b0b96e
Juri Surakka <juri.surakka@eduvantaa.fi>
2020-10-28 12:20:11 +0200

    Temp commit.

--- api.js
+++ api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { asyncMiddleware } = require('./middleware');
 
 let db = require('./db/persistence.js');
+const Player = require('./data/player');
 let data = null;
 
 (async () => {
@@ -24,6 +25,31 @@ router.get('/player/:id', asyncMiddleware(async (req, res) => {
 
         if(result) {
             return res.status(200).send(result);
+        } else {
+            return res.status(404).send();
+        }
+        
+    });  
+}));
+
+router.post('/player/', asyncMiddleware(async (req, res) => {
+
+    const payload = req.body;
+
+    const player = new Player();
+    player.uuid = 'whatevs'
+    player.createdAt = new Date();
+    player.name = payload.name;
+    player.teamName = payload.teamName;
+    player.score = payload.score;
+
+    data.collection('player').insertOne(player, function(err, result) {
+        if (err) {
+            return res.status(500).send(err);
+        };
+
+        if(result) {
+            return res.status(200).send(player);
         } else {
             return res.status(404).send();
         }
