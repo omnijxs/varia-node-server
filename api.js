9e0d8fb786a44c3bab0af6d4d2ad3bae15f0f94c
FLATISHKA <60339162+FLATISHKA@users.noreply.github.com>
2020-11-16 13:33:56 +0200

    crudTest done

--- api.js
+++ api.js
@@ -19,6 +19,63 @@ router.get('/foo', asyncMiddleware(async (req, res) => {
     return res.status(200).send(result);
 }));
 
+router.get("/player/:id", asyncMiddleware(async (req, res) => {
+    const id = req.params.id
+    data.collection('player').findOne({uuid: id}, function(err, result) {
+        if (result){
+            return res.status(200).send(result);
+        }else{
+            return res.status(404).send(result); 
+        }
+    });
+    
+}));
+
+router.post('/player', asyncMiddleware(async (req, res) => {
+    const payload = req.body;
+    const newid = 'ID'
+    const date = new Date()
+    const obj = new Player (newid, payload.name, payload.score, payload.teamName, date)
+    data.collection('player').insertOne(obj, function(err, result) {
+        if (result){
+            return res.status(200).send(obj);
+        }else{
+            return res.status(404).send(obj); 
+        }
+    });
+}));
+
+router.put('/player', asyncMiddleware(async (req, res) => {
+    const payload = req.body;
+    const id = payload.uuid
+    data.collection('player').updateOne({uuid: id},
+        {$set:{'name': payload.name,
+                'score': payload.score,
+                'teamName': payload.teamName}}, function(err, result){  
+        data.collection('player').findOne({uuid: id}, function(err, result){
+            if (result){
+                return res.status(200).send(result);
+            }else{
+                return res.status(404).send(result); 
+            }
+        });
+    });
+}));
+
+router.delete("/player", asyncMiddleware(async (req, res) => {
+    const payload = req.body;
+    const id = payload.uuid;
+    data.collection('player').findOne({uuid: id}, function(err, result) {
+        data.collection('player').deleteOne({uuid: id})
+        console.log(result)
+        if (result){
+            return res.status(200).send(result);
+        }else{
+            return res.status(404).send(result); 
+        }
+    });
+    
+}));
 // Endpoints here
 
 async function setDB(env) {
